204bd0752e9c98d0b0be17d5f1e370ea2555ae8e
startdusk <522383345@qq.com>
2022-03-27 19:16:03 +0800

    feat✨:  add bencode marshal

---
+++ bencode/marshal.go
@@ -0,0 +1,215 @@
+package bencode
+
+import (
+	"errors"
+	"io"
+	"reflect"
+	"strings"
+)
+
+func Unmarshal(r io.Reader, s interface{}) error {
+	o, err := Parse(r)
+	if err != nil {
+		return err
+	}
+	p := reflect.ValueOf(s)
+	if p.Kind() != reflect.Ptr {
+		return errors.New("dest must be a pointer")
+	}
+	switch o.typ {
+	case BLIST:
+		list, _ := o.List()
+		l := reflect.MakeSlice(p.Elem().Type(), len(list), len(list))
+		p.Elem().Set(l)
+		err = unmarshalList(p, list)
+		if err != nil {
+			return err
+		}
+	case BDICT:
+		dict, _ := o.Dict()
+		err = unmarshalDict(p, dict)
+		if err != nil {
+			return err
+		}
+	default:
+		return errors.New("src code must be struct or slice")
+	}
+	return nil
+}
+
+// p.Kind must be Ptr && p.Elem().Type().Kind() must be Slice
+func unmarshalList(p reflect.Value, list []*BObject) error {
+	if p.Kind() != reflect.Ptr || p.Elem().Type().Kind() != reflect.Slice {
+		return errors.New("dest must be pointer to slice")
+	}
+	v := p.Elem()
+	if len(list) == 0 {
+		return nil
+	}
+	switch list[0].typ {
+	case BSTR:
+		for i, o := range list {
+			val, err := o.Str()
+			if err != nil {
+				return err
+			}
+			v.Index(i).SetString(val)
+		}
+	case BINT:
+		for i, o := range list {
+			val, err := o.Int()
+			if err != nil {
+				return err
+			}
+			v.Index(i).SetInt(int64(val))
+		}
+	case BLIST:
+		for i, o := range list {
+			val, err := o.List()
+			if err != nil {
+				return err
+			}
+			if v.Type().Elem().Kind() != reflect.Slice {
+				return ErrTyp
+			}
+			lp := reflect.New(v.Type().Elem())
+			ls := reflect.MakeSlice(v.Type().Elem(), len(val), len(val))
+			lp.Elem().Set(ls)
+			err = unmarshalList(lp, val)
+			if err != nil {
+				return err
+			}
+			v.Index(i).Set(lp.Elem())
+		}
+	case BDICT:
+		for i, o := range list {
+			val, err := o.Dict()
+			if err != nil {
+				return err
+			}
+			if v.Type().Elem().Kind() != reflect.Struct {
+				return ErrTyp
+			}
+			dp := reflect.New(v.Type().Elem())
+			err = unmarshalDict(dp, val)
+			if err != nil {
+				return err
+			}
+			v.Index(i).Set(dp.Elem())
+		}
+	}
+	return nil
+}
+
+// p.Kind() must be Ptr && p.Elem().Type().Kind() must be Struct
+func unmarshalDict(p reflect.Value, dict map[string]*BObject) error {
+	if p.Kind() != reflect.Ptr || p.Elem().Type().Kind() != reflect.Struct {
+		return errors.New("dest must be pointer")
+	}
+	v := p.Elem()
+	for i, n := 0, v.NumField(); i < n; i++ {
+		fv := v.Field(i)
+		if !fv.CanSet() {
+			continue
+		}
+		ft := v.Type().Field(i)
+		key := ft.Tag.Get("bencode")
+		if key == "" {
+			key = strings.ToLower(ft.Name)
+		}
+		fo := dict[key]
+		if fo == nil {
+			continue
+		}
+		switch fo.typ {
+		case BSTR:
+			if ft.Type.Kind() != reflect.String {
+				break
+			}
+			val, _ := fo.Str()
+			fv.SetString(val)
+		case BINT:
+			if ft.Type.Kind() != reflect.Int {
+				break
+			}
+			val, _ := fo.Int()
+			fv.SetInt(int64(val))
+		case BLIST:
+			if ft.Type.Kind() != reflect.Slice {
+				break
+			}
+			list, _ := fo.List()
+			lp := reflect.New(ft.Type)
+			ls := reflect.MakeSlice(ft.Type, len(list), len(list))
+			lp.Elem().Set(ls)
+			err := unmarshalList(lp, list)
+			if err != nil {
+				break
+			}
+			fv.Set(lp.Elem())
+		case BDICT:
+			if ft.Type.Kind() != reflect.Struct {
+				break
+			}
+			dp := reflect.New(ft.Type)
+			dict, _ := fo.Dict()
+			err := unmarshalDict(dp, dict)
+			if err != nil {
+				break
+			}
+			fv.Set(dp.Elem())
+		}
+	}
+	return nil
+}
+
+func marshalValue(w io.Writer, v reflect.Value) int {
+	len := 0
+	switch v.Kind() {
+	case reflect.String:
+		len += EncodeString(w, v.String())
+	case reflect.Int:
+		len += EncodeInt(w, int(v.Int()))
+	case reflect.Slice:
+		len += marshalList(w, v)
+	case reflect.Struct:
+		len += marshalDict(w, v)
+	}
+	return len
+}
+
+func marshalList(w io.Writer, vl reflect.Value) int {
+	len := 2
+	w.Write([]byte{'l'})
+	for i := 0; i < vl.Len(); i++ {
+		ev := vl.Index(i)
+		len += marshalValue(w, ev)
+	}
+	w.Write([]byte{'e'})
+	return len
+}
+
+func marshalDict(w io.Writer, vd reflect.Value) int {
+	len := 2
+	w.Write([]byte{'d'})
+	for i := 0; i < vd.NumField(); i++ {
+		fv := vd.Field(i)
+		ft := vd.Type().Field(i)
+		key := ft.Tag.Get("bencode")
+		if key == "" {
+			key = strings.ToLower(ft.Name)
+		}
+		len += EncodeString(w, key)
+		len += marshalValue(w, fv)
+	}
+	w.Write([]byte{'e'})
+	return len
+}
+
+func Marshal(w io.Writer, s interface{}) int {
+	v := reflect.ValueOf(s)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	return marshalValue(w, v)
+}
---
+++ bencode/marshal_test.go
@@ -0,0 +1,109 @@
+package bencode
+
+import (
+	"bytes"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+type User struct {
+	Name string `bencode:"name"`
+	Age  int    `bencode:"age"`
+}
+
+type Role struct {
+	Id   int
+	User `bencode:"user"`
+}
+
+type Score struct {
+	User  `bencode:"user"`
+	Value []int `bencode:"value"`
+}
+
+type Team struct {
+	Name   string `bencode:"name"`
+	Size   int    `bencode:"size"`
+	Member []User `bencode:"member"`
+}
+
+func TestMarshalBasic(t *testing.T) {
+	buf := new(bytes.Buffer)
+	str := "abc"
+	len := Marshal(buf, str)
+	assert.Equal(t, 5, len)
+	assert.Equal(t, "3:abc", buf.String())
+
+	buf.Reset()
+	val := 199
+	len = Marshal(buf, val)
+	assert.Equal(t, 5, len)
+	assert.Equal(t, "i199e", buf.String())
+}
+
+func TestUnmarshalList(t *testing.T) {
+	str := `li85ei90ei95ee`
+	l := &[]int{}
+	Unmarshal(bytes.NewBufferString(str), l)
+	assert.DeepEqual(t, []int{85, 90, 95}, *l)
+
+	buf := new(bytes.Buffer)
+	length := Marshal(buf, l)
+	assert.Equal(t, len(str), length)
+	assert.Equal(t, str, buf.String())
+}
+
+func TestUnmarshalUser(t *testing.T) {
+	str := `d4:name6:archer3:agei29ee`
+	u := &User{}
+	Unmarshal(bytes.NewBufferString(str), u)
+	assert.Equal(t, "archer", u.Name)
+	assert.Equal(t, 29, u.Age)
+
+	buf := new(bytes.Buffer)
+	length := Marshal(buf, u)
+	assert.Equal(t, len(str), length)
+	assert.Equal(t, str, buf.String())
+}
+
+func TestUnmarshalRole(t *testing.T) {
+	str := `d2:idi1e4:userd4:name6:archer3:agei29eee`
+	r := &Role{}
+	Unmarshal(bytes.NewBufferString(str), r)
+	assert.Equal(t, 1, r.Id)
+	assert.Equal(t, "archer", r.Name)
+	assert.Equal(t, 29, r.Age)
+
+	buf := new(bytes.Buffer)
+	length := Marshal(buf, r)
+	assert.Equal(t, len(str), length)
+	assert.Equal(t, str, buf.String())
+}
+
+func TestUnmarshalScore(t *testing.T) {
+	str := `d4:userd4:name6:archer3:agei29ee5:valueli80ei85ei90eee`
+	s := &Score{}
+	Unmarshal(bytes.NewBufferString(str), s)
+	assert.Equal(t, "archer", s.Name)
+	assert.Equal(t, 29, s.Age)
+	assert.DeepEqual(t, []int{80, 85, 90}, s.Value)
+
+	buf := new(bytes.Buffer)
+	length := Marshal(buf, s)
+	assert.Equal(t, len(str), length)
+	assert.Equal(t, str, buf.String())
+}
+
+func TestUnmarshalTeam(t *testing.T) {
+	str := `d4:name3:ace4:sizei2e6:memberld4:name6:archer3:agei29eed4:name5:nancy3:agei31eeee`
+	team := &Team{}
+	Unmarshal(bytes.NewBufferString(str), team)
+	assert.Equal(t, "ace", team.Name)
+	assert.Equal(t, 2, team.Size)
+
+	buf := new(bytes.Buffer)
+	length := Marshal(buf, team)
+	assert.Equal(t, len(str), length)
+	assert.Equal(t, str, buf.String())
+}
